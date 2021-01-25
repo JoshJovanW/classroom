5d729bee21a5e71f3f66d7652506216100de6a61
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-01-25 19:47:27 +0700

    Create student.py

---
+++ student.py
@@ -0,0 +1,11 @@
+from score import Score
+class Student:
+    def __init__(self, name, id, number):
+        self.name = name
+        self.id = id
+        self.number = number
+        self.scores = Score()
+
+   
+    
+        
