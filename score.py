dca8b420acc96bdf7fb14671c3d5665383d34d7b
JoshJovanW <69958793+JoshJovanW@users.noreply.github.com>
2021-01-31 15:23:02 +0700

    Update score.py

--- score.py
+++ score.py
@@ -2,8 +2,8 @@ class Score:
     def __init__(self):
         self.test = []
         self.quiz = []
-        self.finals = None
-        self.average = None
+        self.finals = 0
+        self.average = 0
 
     def add_test_score(self, score):
         if score > 0 and score <= 100:
@@ -19,7 +19,13 @@ class Score:
             self.finals = score
 
     def find_average(self):
-        average_of_test = sum(self.test)/len(self.test)
-        average_of_quiz = sum(self.quiz)/len(self.quiz)
+        average_of_test = 0
+        if len(self.test) != 0:
+            average_of_test = sum(self.test)/len(self.test) 
+
+        average_of_quiz = 0
+        if len(self.quiz) != 0:
+            average_of_quiz = sum(self.quiz)/len(self.quiz)
+
         total_average_score = (average_of_quiz * 0.1) + (average_of_test * 0.5) + (self.finals * 0.4)
         self.average = total_average_score
